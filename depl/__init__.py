894265420a3c7df00acd7e42f8d3c6b9e92adb9e
David Halter <davidhalter88@gmail.com>
2013-09-23 14:42:37 +0430

    basic docopt usage

--- depl/__init__.py
+++ depl/__init__.py
@@ -1,1 +1,21 @@
+"""
+depl - deploy easy and fast.
+Deploying stuff is hard, managing nginx and postgres painful, why not the easy
+way?
+
+Usage:
+  depl deploy [-c=<file>] [<host>..]
+  depl -h | --help
+
+Options:
+  -c, --config=<file>   Deploy configuration file [default: .depl.yml]
+"""
+
+import docopt
+
 __version__ = '0.0.1'
+
+
+def main():
+    args = docopt.docopt(__doc__, version=__version__)
+    print args
