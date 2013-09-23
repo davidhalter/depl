1166588f1ce3158a64305af55258fee4e226287e
David Halter <davidhalter88@gmail.com>
2013-09-24 00:34:54 +0430

    validation tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,6 +1,18 @@
+import textwrap
+
 import pytest
 
 from depl import config
+
+
+def validate(tmpdir, code, fail):
+    p = tmpdir.join("default.yml")
+    p.write(textwrap.dedent(code))
+    if fail:
+        with pytest.raises(config.ValidationError):
+            config.Config(str(p), [])
+    else:
+        assert config.Config(str(p), [])
 
 
 def test_not_existing(tmpdir):
@@ -9,12 +21,19 @@ def test_not_existing(tmpdir):
         config.Config(p, [])
 
 
-def test_simple(tmpdir):
-    p = tmpdir.join("default.yml")
-    p.write("""
+def test_simple_valid(tmpdir):
+    s = """
     deploy:
       - django
       - reddis
-    """)
+    """
+    validate(tmpdir, s, 1)
 
-    config.Config(str(p), [])
+
+def test_simple_invalid(tmpdir):
+    s = """
+    deploy:
+      django
+      reddis
+    """
+    validate(tmpdir, s, 1)
