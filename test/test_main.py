13649e2817de4784e46a5473c940954208da4bf4
David Halter <davidhalter88@gmail.com>
2013-09-27 13:29:46 +0430

    integration test for wrong config files

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,6 @@
 import textwrap
 import subprocess
+import os
 
 import pytest
 
@@ -11,12 +12,30 @@ def run(cmd):
     return stdout
 
 
-def config_file(tmpdir, code, file_name = "default.yml"):
-    p = tmpdir.join(file_name)
-    p.write(textwrap.dedent(code))
+def config_file(code, file_name = "depl.yml"):
+    def decorator(func):
+        def wrapper(tmpdir):
+            p = tmpdir.join(file_name)
+            p.write(textwrap.dedent(code))
+            path = os.getcwd()
+            try:
+                print str(tmpdir)
+                os.chdir(str(tmpdir))
+                return func()
+            finally:
+                os.chdir(path)
+        return wrapper
+    return decorator
 
 
 def test_no_config():
     with pytest.raises(subprocess.CalledProcessError) as excinfo:
         run('depl deploy')
     assert excinfo.value.returncode == 1
+
+
+@config_file('a wrong config file')
+def test_wrong_config():
+    with pytest.raises(subprocess.CalledProcessError) as excinfo:
+        run('depl deploy')
+    assert excinfo.value.returncode == 2
