ba4b075a8236c27fe55af6e6b160d6a1b9abfdd4
David Halter <davidhalter88@gmail.com>
2013-09-29 16:33:59 +0430

    integration test for wrong config files

--- depl/utils.py
+++ depl/utils.py
@@ -2,6 +2,11 @@ from fabric.api import local, run as run_fabric, env
 
 
 def run(*args, **kwargs):
+    """
+    TODO This is very ugly! local/run don't have the same interfaces, but
+    leave it for now - testing reasons. (btw: this should be done by fabric
+    anyways, there's a ticket for it)
+    """
     if len(env.hosts) and env.hosts[0] in ['localhost', '127.0.0.1']:
         return local(*args, **kwargs)
     else:
