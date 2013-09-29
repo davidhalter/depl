d2659f94a305334fe38f1a72fa93529f9265e46e
David Halter <davidhalter88@gmail.com>
2013-09-29 16:31:42 +0430

    more hosts unit tests

---
+++ test/test_hosts.py
@@ -0,0 +1,20 @@
+from depl import config
+from depl import hosts
+
+
+def test_run(tmpdir):
+    pool = config.Pool(None, [config.Host('localhost')],
+                             [config.Deploy('redis', {'id': 'redis'})])
+
+    hosts.run_in_pool(pool, ['ls'])
+
+
+def test_deploy(monkeypatch):
+    def mockreturn(pool, commands):
+        commands = list(commands)
+        assert len(commands) >= 1 and 'redis' in commands[0]
+    monkeypatch.setattr(hosts, 'run_in_pool', mockreturn)
+
+    pool = config.Pool(None, [config.Host('localhost')],
+                             [config.Deploy('redis', {'id': 'redis'})])
+    hosts.deploy_pool(pool)
