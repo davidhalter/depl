130d1e454e9608aab6ce2fac9d63b86a8ce47ba7
David Halter <davidhalter88@gmail.com>
2013-09-30 15:13:41 +0430

    django is now the same as python with a wsgi default

--- depl/deploy/django.py
+++ depl/deploy/django.py
@@ -2,6 +2,4 @@ from depl.deploy import python
 
 
 def load(settings, package):
-    commands = []
-    python.load(settings, package)
-    return ['pip'], commands
+    return python.load(settings, package)
