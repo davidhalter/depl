fc0225b84f9205e4ff8d0cdcf66bf8e591ac3a77
David Halter <davidhalter88@gmail.com>
2013-10-01 01:08:16 +0430

    fix a test - read instead of getcode

--- test/deploy/test_python.py
+++ test/deploy/test_python.py
@@ -14,4 +14,4 @@ def test_flask_simple(tempdir):
     flask_path = join(dirname(abspath(__file__)), 'sample', 'flask')
     move_dir_content(flask_path, str(tempdir))
     main_run(['depl', 'deploy', 'localhost'])
-    assert urllib.urlopen("http://localhost:8888/").getcode() == "Hello World!"
+    assert urllib.urlopen("http://localhost:8888/").read() == "Hello World!"
