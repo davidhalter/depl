a2fdba8efbe67ce4f336f8eb5dc70415d04caf4b
David Halter <davidhalter88@gmail.com>
2013-10-01 23:59:32 +0430

    postgres automatic configuration

--- depl/deploy/postgresql.py
+++ depl/deploy/postgresql.py
@@ -1,3 +1,19 @@
+import textwrap
+
+from fabric.api import sudo
+
+
 def load(settings, package):
-    # pg dependency, no commands
-    return ['postgresql'], []
+    for need in ['user', 'password', 'database']:
+        if settings[need] is None:
+            raise ValueError('You need to define a %s to deploy postgres.' % need)
+
+    sql = textwrap.dedent("""
+    CREATE DATABASE {database};
+    CREATE USER {user} WITH PASSWORD '{password}';
+    GRANT ALL PRIVILEGES ON DATABASE '{database}' to {user];
+    """).format(**settings)
+
+    def setup_user():
+        sudo('psql -c "%s"' % sql.replace('"', r'\"'), user='postgres')
+    return ['postgresql'], [setup_user]
