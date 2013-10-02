1d4360bde2ce1bda46fd76773289058bbad3b510
David Halter <davidhalter88@gmail.com>
2013-10-02 11:12:40 +0430

    postgres: only create db/user if they don't exist already

--- depl/deploy/postgresql.py
+++ depl/deploy/postgresql.py
@@ -16,8 +16,19 @@ def load(settings, package):
     GRANT ALL PRIVILEGES ON DATABASE {database} to {user};
     """).format(**settings)
 
+    db_exists = "select datname from pg_catalog.pg_database where datname='%s'"
+    user_exists = "select usename from pg_catalog.pg_user where usename='%s'"
+
     def setup_user():
+        def psql_command(sql):
+            return sudo('psql -c "%s"' % sql.replace('"', r'\"'), user='postgres')
+
+        def no_result(sql):
+            return '(0 rows)' in psql_command(sql)
+
         sudo('service postgresql start')
-        sudo('psql -c "%s"' % create_db.replace('"', r'\"'), user='postgres')
-        sudo('psql -c "%s"' % create_user.replace('"', r'\"'), user='postgres')
+        if no_result(db_exists % settings['database']):
+            psql_command(create_db)
+        if no_result(user_exists % settings['user']):
+            psql_command(create_user)
     return ['postgresql'], [setup_user]
