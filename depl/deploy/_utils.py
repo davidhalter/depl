196529a57bd4de3e8fb55355d2f6eba8f116b039
David Halter <davidhalter88@gmail.com>
2013-11-18 10:07:57 +0100

    nginx port documentation

--- depl/deploy/_utils.py
+++ depl/deploy/_utils.py
@@ -1,3 +1,14 @@
+"""
+
+Nginx Configuration
+-------------------
+
+Nginx can typically be configured with two simple https/http port settings. If
+you set one of the port to ``REDIRECT``, it will redirect to the other
+protocol. You can also disable the protocol by setting the port number to
+``false``.
+
+"""
 import os
 import textwrap
 
@@ -5,7 +16,7 @@ from fabric.api import put, sudo
 from fabric.contrib.project import upload_project
 
 SSL_PATH = '/etc/nginx/ssl'
-SSL_FILE_PATH = '%s/depl_%s' % (SSL_PATH, depl_id)
+SSL_FILE_PATH = '%s/depl_%%s' % SSL_PATH
 
 
 def lazy(func):
@@ -15,6 +26,7 @@ def lazy(func):
 
 
 def nginx_config(settings, locations):
+    ssl_file_path = SSL_FILE_PATH % settings['id']
     config = """
         server {
             listen      [::]:%s;
@@ -33,7 +45,7 @@ def nginx_config(settings, locations):
     add_ssl = """
     ssl_certificate %s
     ssl_certificate_key %s
-    """ % (SSL_FILE_PATH + '.crt', SSL_FILE_PATH + '.key')
+    """ % (ssl_file_path + '.crt', ssl_file_path + '.key')
 
     l = '    location %s {%s}'
     locations = [l % (path, values) for path, values in locations.items()]
@@ -43,8 +55,10 @@ def nginx_config(settings, locations):
     cfgs = (settings['port'], add_http), (ssl['port'] + ' ssl', add_ssl)
     config_txt = ''
     for port, add_config in cfgs:
-        config_txt += textwrap.dedent(config) % (port, settings['url'],
-                                                 add_config, locations)
+        # if port is not set - disable it
+        if port:
+            config_txt += textwrap.dedent(config) % (port, settings['url'],
+                                                     add_config, locations)
     return config_txt
 
 
@@ -75,6 +89,7 @@ def move_project_to_www(local_path, remote_path):
 
 @lazy
 def generate_ssl_keys(depl_id, ssl_config):
+    ssl_file_path = SSL_FILE_PATH % depl_id
     # The whole generation thing is a proof of concept to have ssl running
     # without a CA. This makes it easier to deploy things the first time.
     # So keep in mind: This **doesn't protect you against "Man in the Middle"**
@@ -86,13 +101,13 @@ def generate_ssl_keys(depl_id, ssl_config):
     sudo('mkdir %s || true' % SSL_PATH)
     # generate key
     sudo('openssl genrsa -des3 -out {1}.key.orig 1024{2}'
-         .format(SSL_FILE_PATH, pass_out))
+         .format(ssl_file_path, pass_out))
     # generate certificate signing request
     sudo('echo -e "\n\n\n\n\n\n\n\n" | '
          'openssl req -new -key {0}.key.orig -out {0}.csr {1}'
-         .format(SSL_FILE_PATH, pass_in))
+         .format(ssl_file_path, pass_in))
     # remove password from key
     sudo('openssl rsa -in {0}.key.orig -out {0}.key {1}'
-         .format(SSL_FILE_PATH, pass_in))
+         .format(ssl_file_path, pass_in))
     sudo('openssl x509 -req -days 9999 -in {0}.csr -signkey {0}.key -out {0}.crt'
-         .format(SSL_FILE_PATH))
+         .format(ssl_file_path))
