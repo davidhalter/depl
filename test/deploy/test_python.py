3640d6f50d28f15c074d589bf3314f052c2deda1
David Halter <davidhalter88@gmail.com>
2013-11-18 22:35:26 +0100

    redirect test

--- test/deploy/test_python.py
+++ test/deploy/test_python.py
@@ -6,6 +6,13 @@ import requests
 from test_main import config_file, move_dir_content, main_run
 
 
+def flask_depl(tmpdir):
+    flask_path = join(dirname(abspath(__file__)), 'sample', 'flask')
+    move_dir_content(flask_path, str(tmpdir))
+    main_run(['depl', 'deploy', 'localhost'])
+    return "Hello World!"  # http resulting text
+
+
 @config_file('''
     deploy:
         - python:
@@ -15,10 +22,7 @@ from test_main import config_file, move_dir_content, main_run
                 port: 444
     ''')
 def test_flask_simple(tmpdir):
-    flask_path = join(dirname(abspath(__file__)), 'sample', 'flask')
-    move_dir_content(flask_path, str(tmpdir))
-    main_run(['depl', 'deploy', 'localhost'])
-    txt = "Hello World!"
+    txt = flask_depl(tmpdir)
     assert requests.get("http://localhost:8888/").text == txt
     # ipv4
     assert requests.get("http://127.0.0.1:8888/").text == txt
@@ -27,8 +31,27 @@ def test_flask_simple(tmpdir):
         # broken stdlib causes this test to not work in py26:
         # https://github.com/shazow/urllib3/pull/203
         assert requests.get("http://[::1]:8888/").text == txt
-        assert requests.get("https://[::1]:444/").text == txt
+        assert requests.get("https://[::1]:444/", verify=False).text == txt
 
-    r = requests.get("https://127.0.0.1:444/") == txt
+    r = requests.get("https://127.0.0.1:444/", verify=False)
     assert r.text == txt
     assert r.history == []
+
+
+@config_file('''
+    deploy:
+        - python:
+            port: 8888
+            wsgi: hello:app
+            ssl:
+                port: REDIRECT
+    ''')
+def test_flask_redirects(tmpdir):
+    txt = flask_depl(tmpdir)
+    r = requests.get("http://localhost:8888/")
+    assert r.text == txt
+    assert r.history == []
+
+    r = requests.get("https://127.0.0.1:444/", verify=False)
+    assert r.text == txt
+    assert len(r.history) == 1 and r.history[0].status_code == 301
