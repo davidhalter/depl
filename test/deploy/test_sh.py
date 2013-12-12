08e5e94d07aa3fc487d1f0f3338912b060a1f020
David Halter <davidhalter88@gmail.com>
2013-12-12 14:23:44 +0100

    sh testing

---
+++ test/deploy/test_sh.py
@@ -0,0 +1,31 @@
+from ..helpers import config_file, main_run
+
+
+@config_file('''
+    deploy:
+        - sh: |
+           set -e
+           touch .depl_sh
+           rm depl_sh
+    ''')
+def test_no_error(tmpdir):
+    main_run(['depl', 'deploy', 'localhost'])
+
+
+@config_file('''
+    deploy:
+        - sh: |
+           set -e
+           rm depl_a
+    ''')
+def test_raise_error(tmpdir):
+    main_run(['depl', 'deploy', 'localhost'])
+
+
+@config_file('''
+    deploy:
+        - sh: |
+           rm depl_a
+    ''')
+def test_raise_error_but_quiet(tmpdir):
+    main_run(['depl', 'deploy', 'localhost'])
