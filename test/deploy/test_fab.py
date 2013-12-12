3f7e7dfd42933e8d9143e29953015911e7402885
David Halter <davidhalter88@gmail.com>
2013-12-12 17:21:06 +0100

    small replacement

--- test/deploy/test_fab.py
+++ test/deploy/test_fab.py
@@ -6,8 +6,8 @@ from ..helpers import config_file, main_run
 @config_file('''
     deploy:
         - fab: |
-           run('touch depl_sh')
-           sudo("rm depl_sh")
+           run('touch depl_fab')
+           sudo("rm depl_fab")
     ''')
 def test_no_error(tmpdir):
     main_run(['depl', 'deploy', 'localhost'])
@@ -16,7 +16,7 @@ def test_no_error(tmpdir):
 @config_file('''
     deploy:
         - fab: |
-           sudo("rm depl_sh")
+           sudo("rm depl_fab")
     ''')
 def test_raise_error(tmpdir):
     with pytest.raises(SystemExit):
@@ -27,7 +27,7 @@ def test_raise_error(tmpdir):
     deploy:
         - fab: |
            with warn_only():
-               result = sudo("rm depl_sh")
+               result = sudo("rm depl_fab")
                assert result.failed
     ''')
 def test_raise_error_but_quiet(tmpdir):
