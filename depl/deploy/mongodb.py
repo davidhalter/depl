afa8d0a383433a871615a56c358b4f52d45f6e1a
David Halter <davidhalter88@gmail.com>
2013-12-13 16:39:18 +0100

    better mongodb documentation

--- depl/deploy/mongodb.py
+++ depl/deploy/mongodb.py
@@ -1,7 +1,15 @@
 """
-MongoDB is very easy to setup - but there remains one glitch, by default the
-mongodb port 27017 is open. Maybe add ``bind_ip = 127.0.0.1`` to
-``/etc/mongodb.conf``?
+MongoDB is very easy to setup. There are no configuration options, just mention
+it:
+
+.. sourcecode: yaml
+
+    deploy:
+        - mongodb
+
+There remains one glitch, by default the mongodb port 27017 is open. Maybe add
+``bind_ip = 127.0.0.1`` to ``/etc/mongodb.conf``? I have no idea how insecure
+it is, but it seems to be an open door.
 """
 from fabric.api import sudo
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,3 +1,5 @@
+.. include:: global.rst
+
 depl - deploying should be easy
 ===============================
 
